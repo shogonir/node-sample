8055e28f848311027324d12388963c55a72cbbc0
shogonir <shogonir@gmail.com>
2017-04-23 17:11:49 +0900

    [add] babel preset es2016

--- src/application.js
+++ src/application.js
@@ -16,3 +16,16 @@ let shogonir = new Friend('Shogo N');
 
 shogonir.callName();
 
+
+// test es2016 syntax
+
+let power = 3 ** 3;
+
+console.log('3 ** 3 = ' + power);
+
+let array = [1, 2, 3];
+
+console.log('1 in array? : ' + array.includes(1));
+
+console.log('0 in array? : ' + array.includes(0));
+
